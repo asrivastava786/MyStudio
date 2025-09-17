b67c3653bed1ea91682089c71385fdc55eb2bc45
Akash <asrivastava636@gmail.com>
2025-09-17 22:43:24 +0200

    runtime edge

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,8 @@ import "./globals.css";
 import Footer from "@/components/ui/Footer";
 import Header from "@/components/ui/Header";
 
+export const runtime = "edge"; 
+
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
